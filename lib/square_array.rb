c7d780167ae56da50624f9845f943b8af5a3d25a
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-24 20:35:38 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+def square array[1,2,3,4]
+   square array(1,4,9,16)
+   
+   # =>[1,4,9,16]
+   
+  def square array[4,6,9,10]
+    square array(16.36,81,100)
+   
+   
+   #=>[16,36,81,100]
 end
